037a6b1ffe59afa1112fd2c74a472b6a85f706c8
Carlos Padilla <cpadillaortega@gmail.com>
2024-11-13 17:39:34 -0400

    finished

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
-//Getting numbers
+// Getting numbers
 var number1 = parseInt(prompt("Enter the first number", 0));
 var number2 = parseInt(prompt("Enter the second number", 0));
 
-//data type verification
+// Data type verification
 while (number1 < 0 || isNaN(number1) || number2 < 0 || isNaN(number2)) {
-  //We already defined the variables
+  // We already defined the variables
   number1 = parseInt(prompt("Enter the first number", 0));
   number2 = parseInt(prompt("Enter the second number", 0));
 }
 
 var result =
-  "Addition is: " +
-  (number1 + number2) +
+  "Addition is: " + (number1 + number2) +
   "<br/>" +
-  "Subtraction is: " +
-  (number1 - number2) +
+  "Subtraction is: " + (number1 - number2) +
   "<br/>" +
-  "division is: " +
-  (number1 / number2) +
+  "Division is: " + (number1 / number2) +
   "<br/>" +
-  "multiplication is: " +
-  (number1 * number2);
+  "Multiplication is: " + (number1 * number2);
 
 document.write(result);
+
+var CMDresult =
+  "Addition is: " + (number1 + number2) +
+  "\n" +
+  "Subtraction is: " + (number1 - number2) +
+  "\n" +
+  "Division is: " + (number1 / number2) +
+  "\n" +
+  "Multiplication is: " + (number1 * number2);
+
+alert(CMDresult);
+console.log(CMDresult);
