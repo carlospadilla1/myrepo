9e7a0f41f894da25c57697d25d82733d326cbbb9
Carlos Padilla <cpadillaortega@gmail.com>
2024-11-13 16:01:11 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+//Getting numbers
+var number1 = parseInt(prompt("Enter the first number", 0));
+var number2 = parseInt(prompt("Enter the second number", 0));
+
+//data type verification
+while (number1 < 0 || isNaN(number1) || number2 < 0 || isNaN(number2)) {
+    //We already defined the variables
+    number1 = 
+}
